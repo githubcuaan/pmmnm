b852910e643e0bc2a6e3a14f7481452667ae71df
dnhfan <dinhan.0321@gmail.com>
2025-12-04 03:21:21 +0700

    refactor(maps): extract type

---
+++ components/Maps/types.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 PKA-OpenLD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface Zone {
+  id: string;
+  type: "flood" | "outage";
+  shape: "circle" | "line";
+  center?: number[]; // [lng, lat] for circles (zones)
+  radius?: number; // in meters for circles (zones)
+  coordinates?: number[][]; // for lines (routes/paths)
+  riskLevel?: number;
+  title?: string;
+  description?: string;
+}
+
+export interface Sensor {
+  id: string;
+  name: string;
+  location: [number, number];
+  type: "water_level" | "temperature" | "humidity";
+  threshold: number;
+  actionType: "flood" | "outage";
+  createdAt?: number;
+}
